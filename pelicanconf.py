61ba5af79df015de630fb2544f800f185007a679
bussiere <bussiere@gmail.com>
2012-11-28 08:33:52 +0100

    rajout
    rajout

---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Bussiere & Camille'
+SITENAME = u'A Fez Odyssey'
+SITEURL = 'http://bussiere.github.com/fez_odissey'
+
+TIMEZONE = 'Europe/Paris'
+FEED_DOMAIN = 'http://bussiere.github.com/fez_odissey'
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
